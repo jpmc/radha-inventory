7e22a2bb4a888b9606a7caa977bf4d69dd688aaf
James Corrao <jayissmarter@gmail.com>
2023-03-31 01:55:45 -0400

    Initial templates and base.

---
+++ inventory.js
@@ -0,0 +1,31 @@
+const inventory = [
+  {
+    "name": "Days of Rations",
+    "unit_weight": 2,
+    "value": null,
+    "location": [{
+      "name": "Bag of Holding",
+      "quantity": 16,
+    }],
+    "type": "Consumable",
+    "notes": null,
+    "links": []
+  },
+  {
+    "name": "Healers Kit",
+    "unit_weight": 3,
+    "value": 5,
+    "location": [
+        {
+          "name": "Toolbelt",
+          "quantity": 1,
+        }, {
+          "name": "Tavern",
+          "quantity": 1,
+        }
+    ],
+    "type": "Consumable",
+    "notes": "Each kit has ten uses. As an action, you can expend one use of the kit to stabilize a creature that has 0 Hit Points, without needing to make a WIS (Medicine) check.",
+    "links": []
+  },
+]
